ecb7e6f954401e5f2728372f2e017edc58266e1d
jorgezamy <jorge.zamora60@hotmail.com>
2024-07-26 22:41:00 -0600

    feat: creating the routes with the views components created before

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { CreateComponent } from './views/create/create.component';
+import { EditComponent } from './views/edit/edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'edit', component: EditComponent },
+];

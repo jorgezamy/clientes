077d4a7daeb3fd503b74422a555202c4e45a16f2
jorgezamy <jorge.zamora60@hotmail.com>
2024-07-27 20:57:09 -0600

    feat: adding validations to loginForm and getting data introduced it

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm = new FormGroup<ILoginForm>({
+    usuario: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  onLogin() {
+    if (this.loginForm.valid) {
+      const formValue = this.loginForm.value;
+      console.log(formValue);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
+interface ILoginForm {
+  usuario: FormControl<string | null>;
+  password: FormControl<string | null>;
 }

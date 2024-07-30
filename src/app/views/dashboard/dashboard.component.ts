3c9da1f0f169fc83c36ab787aa8d3be5b7b8cfd1
jorgezamy <jorge.zamora60@hotmail.com>
2024-07-29 21:16:30 -0600

    feat: creating a shell for header and footer layout and adding into dashboard. Import in imports was neccessary to do it.

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../layouts/header/header.component';
+import { FooterComponent } from '../../layouts/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, HeaderComponent, FooterComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}

d89092a1e31adf408df62da5c49cc1742f75c71b
jorgezamy <jorge.zamora60@hotmail.com>
2024-07-30 18:48:52 -0600

    feat: gettin the id param from url and gettin token from localstorage

--- src/app/views/edit/edit.component.ts
+++ src/app/views/edit/edit.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { HeaderComponent } from '../../layouts/header/header.component';
+import { FooterComponent } from '../../layouts/footer/footer.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
+  styleUrl: './edit.component.css',
 })
 export class EditComponent {
+  constructor(private _route: Router, private _activedRouter: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    let customerId = this._activedRouter.snapshot.paramMap.get('id');
+    let token = this.getToken();
+    console.log('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
 }
